748fe6c45c71290f41a8d60fcc0767e68bd6c17c
zMeFz <saigakj@inbox.ru>
2016-10-15 13:11:23 +0300

    Add condition form

--- src/server/app/forms.py
+++ src/server/app/forms.py
@@ -1,11 +1,14 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, FileField, BooleanField
+from wtforms import TextField, FileField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Required, Length
 from models import User, Lab, Task
 
 class LoginForm(Form):
-    number = TextField('number', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
 
 class TaskSendForm(Form):
     task = FileField('task', validators = [Required()])
+
+class ConditionForm(Form):
+    name = TextField('name', validators = [Required()])
+    condition = TextAreaField('condition', validators = [Required()])
