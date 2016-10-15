ae1e7013443ef9eba97f2c992ec2461016966bbd
zMeFz <saigakj@inbox.ru>
2016-10-15 15:28:55 +0300

    Bugfix. Attempts time gmt fix

--- src/server/app/forms.py
+++ src/server/app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Required, Length
 from models import User, Lab, Task
 
 class LoginForm(Form):
+    number = TextField('number', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
 
 class TaskSendForm(Form):
