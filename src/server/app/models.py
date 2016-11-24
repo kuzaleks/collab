93c5ea166fc351825ffcee120ac78cd931a1567b
zMeFz <saigakj@inbox.ru>
2016-11-24 15:08:33 +0300

    Group model added.

--- src/server/app/models.py
+++ src/server/app/models.py
@@ -3,33 +3,86 @@ from app import db
 from hashlib import md5
 from config import ROLE_USER, ROLE_PREPOD, ROLE_ADMIN, PART_A, PART_B, STATUS
 
-class User(db.Model, UserMixin):
-	id = db.Column(db.Integer, primary_key = True)
-	cart = db.Column(db.Integer)
+association_table = db.Table('association', db.metadata,
+	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+	db.Column('group_id', db.Integer, db.ForeignKey('group.id'))
+)
 
+class User(db.Model, UserMixin):
+
+	# For both teacher and student
+	id = db.Column(db.Integer, primary_key = True)
+	
 	fname = db.Column(db.String(50))
 	lname = db.Column(db.String(50))
 	pname = db.Column(db.String(50))
 
-	course = db.Column(db.Integer)
-	group = db.Column(db.Integer)
+	role = db.Column(db.Integer, default=ROLE_USER)
+
+	# For student
+	cart = db.Column(db.Integer)
 	
 	variant = db.Column(db.Integer)
 	attempts = db.relationship('Attempt', backref = 'author', lazy = 'dynamic')
-	
-	role = db.Column(db.Integer, default=ROLE_USER)
-	keyword = db.Column(db.String(50), default="") # for prepod
+
+	# For teacher
+	keyword = db.Column(db.String(50), default="")
+	groups = db.relationship("Group", secondary=association_table, back_populates="users")
 
 	def __repr__(self):
-		return '<User %r>' % (self.cart)
+		return '<User %r>' % (self.lname)
 
 	def is_authenticated(self):
 		return True
+
+	def get_course(self):
+		if len(self.groups) == 1 and not self.is_prepod():
+			return self.groups[0].course
+
+	def get_group(self):
+		if len(self.groups) == 1 and not self.is_prepod():
+			return self.groups[0].group_num
+
+	def groups_to_string(self):
+		return ", ".join("%s/%s" % (group.course, group.group_num) for group in self.groups)
+
+	def clear_groups_list(self):
+		self.groups = []
+
+	def update_groups_list(self, groups_string):
+		self.clear_groups_list()
+		groups = [(s.strip().split('/')) for s in groups_string.split(",")]
+		for _course, _group in groups:
+			group = Group.query.filter_by(course=_course, group_num=_group).first()
+			if group:
+				self.groups.append(group)
+
+	def set_group(self, course, group_num):
+		self.clear_groups_list()
+		group = Group.query.filter_by(course=course, group_num=group_num).first()
+		if group:
+			self.groups.append(group)
 
 	def is_prepod(self):
 		if self.role == ROLE_PREPOD or self.role == ROLE_ADMIN:
 			return True
 		return False
+
+class Group(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+
+	course = db.Column(db.Integer)
+	group_num = db.Column(db.Integer)
+
+
+	users = db.relationship(
+		"User",
+		secondary=association_table,
+		back_populates="groups")
+
+	def __repr__(self):
+		return '<Group %r/%r>' % (self.course, self.group_num)
+
 
 class Lab(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
--- src/server/app/views.py
+++ src/server/app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required, UserMixin
 from app import app, db, lm
 from forms import LoginForm, TaskSendForm, ConditionForm, UserUpdateForm
-from models import Lab, User, Task, Attempt
+from models import Lab, User, Task, Attempt, Group
 from datetime import datetime
 from utils import save, check_task, path_generate, get_status, users_tasks, clear_dir, dirs_create
 from config import STATUS, ROLE_USER, ROLE_PREPOD, ROLE_ADMIN
@@ -13,14 +13,14 @@ import collections
 
 """
 Routes:
-    /
-    /index
-    /login
-    /user/<int:cart>
-    /task/<int:task_id>
-    /add_user
-    /edit
-    /add_task
+	/
+	/index
+	/login
+	/user/<int:cart>
+	/task/<int:task_id>
+	/add_user
+	/edit
+	/add_task
 """
 
 @lm.user_loader
@@ -29,289 +29,318 @@ def get_user(ident):
 
 @app.before_request
 def before_request():
-    g.user = current_user
+	g.user = current_user
 
 @app.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('index'))
+	logout_user()
+	return redirect(url_for('index'))
 
 @app.route('/', methods = ['GET', 'POST'])
 @app.route('/index', methods = ['GET', 'POST'])
 @login_required
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    if g.user and g.user.is_authenticated:
-        return redirect(url_for('index'))
-    
-    form = LoginForm()
-    
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        user = User.query.filter_by(keyword = form.number.data, lname = form.lastname.data).first()
-        
-        if not user:
-            try:
-                user = User.query.filter_by(cart = int(form.number.data), lname = form.lastname.data).first()
-            except Exception as e:
-                flash("User not found.")
-                return render_template('login.html', 
-                    title = 'Sign In',
-                    form = form)
-        
-        # user was found
-        return after_login(user, form.number.data)
+	if g.user and g.user.is_authenticated:
+		return redirect(url_for('index'))
+	
+	form = LoginForm()
+	
+	if form.validate_on_submit():
+		session['remember_me'] = form.remember_me.data
+		user = User.query.filter_by(keyword = form.number.data, lname = form.lastname.data).first()
+		
+		if not user:
+			try:
+				user = User.query.filter_by(cart = int(form.number.data), lname = form.lastname.data).first()
+			except Exception as e:
+				flash("User not found.")
+				return render_template('login.html', 
+					title = 'Sign In',
+					form = form)
+		
+		# user was found
+		return after_login(user, form.number.data)
 
-    return render_template('login.html', 
-        title = 'Sign In',
-        form = form)
+	return render_template('login.html', 
+		title = 'Sign In',
+		form = form)
 
 def after_login(user, number):
-    if user is None:
-        flash("User not found.")
-        return redirect(url_for('login'))
+	if user is None:
+		flash("User not found.")
+		return redirect(url_for('login'))
 
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember = remember_me)
+	remember_me = False
+	if 'remember_me' in session:
+		remember_me = session['remember_me']
+		session.pop('remember_me', None)
+	login_user(user, remember = remember_me)
 
-    return redirect(request.args.get('next') or url_for('index'))
+	return redirect(request.args.get('next') or url_for('index'))
 
 @app.route('/user/<int:cart>', methods = ['GET', 'POST'])
 @login_required
 def user(cart):
-    user = User.query.filter_by(cart = cart).first()
-    if not user:
-        return redirect(url_for('index'))        
+	user = User.query.filter_by(cart = cart).first()
+	if not user:
+		return redirect(url_for('index'))        
 
-    table = merge(Task.query.all(), Lab.query.all())
-    table_width = biggest_lab_length(table)
+	table = merge(Task.query.all(), Lab.query.all())
+	table_width = biggest_lab_length(table)
 
-    return render_template('user.html',
-        user = user,
-        attempts = Attempt.query.filter_by(user_id = user.id).all()[::-1],
-        table = table,
-        table_width = table_width,
-        get_status = get_status,
-        users_tasks = users_tasks)
+	return render_template('user.html',
+		user = user,
+		attempts = Attempt.query.filter_by(user_id = user.id).all()[::-1],
+		table = table,
+		table_width = table_width,
+		get_status = get_status,
+		users_tasks = users_tasks)
 
 @app.route('/all_attempts')
 @login_required
 def all_attempts():
-    attempts = Attempt.query.filter_by(user_id = g.user.id).all()
+	attempts = Attempt.query.filter_by(user_id = g.user.id).all()
 
-    return render_template('all_attempts.html',
-        attempts = reversed(attempts),
-        get_status = get_status)
+	return render_template('all_attempts.html',
+		attempts = reversed(attempts),
+		get_status = get_status)
 
 @app.route('/all_users')
 @login_required
 def all_users():
-    if not g.user.is_prepod():
-        flash('You are just user.')
-        return redirect(url_for('index'))
+	if not g.user.is_prepod():
+		flash('You are just user.')
+		return redirect(url_for('index'))
 
-    users = User.query.filter_by(role = ROLE_USER).all()
+	users = User.query.filter_by(role = ROLE_USER).all()
 
-    table = merge(Task.query.all(), Lab.query.all())
-    table_width = biggest_lab_length(table)
+	table = merge(Task.query.all(), Lab.query.all())
+	table_width = biggest_lab_length(table)
 
-    return render_template('users_table.html',
-        users = users,
-        table = table,
-        get_status = get_status,
-        table_width = table_width,
-        users_tasks = users_tasks)
+	return render_template('users_table.html',
+		users = users,
+		table = table,
+		get_status = get_status,
+		table_width = table_width,
+		users_tasks = users_tasks)
 
 @app.route('/choose_task')
 @login_required
 def choose_task():
-    table = merge_obj(Task.query.all(), Lab.query.all())
-    table = collections.OrderedDict(sorted(table.items()))
+	table = merge_obj(Task.query.all(), Lab.query.all())
+	table = collections.OrderedDict(sorted(table.items()))
 
-    return render_template('choose_task.html', table = table)
+	return render_template('choose_task.html', table = table)
 
 @app.route('/task/<int:task_id>', methods=('GET', 'POST'))
 @login_required
 def task(task_id):
-    task = Task.query.filter_by(id = task_id).first()
+	task = Task.query.filter_by(id = task_id).first()
 
-    if not task:
-        return redirect('/choose_task')
-    
-    filepath = "tasks/" + str(task_id) + ".html"
-    full_filepath = "app/templates/" + filepath
+	if not task:
+		return redirect('/choose_task')
+	
+	filepath = "tasks/" + str(task_id) + ".html"
+	full_filepath = "app/templates/" + filepath
 
-    taskSendForm = TaskSendForm()
-    conditionForm = ConditionForm()
+	taskSendForm = TaskSendForm()
+	conditionForm = ConditionForm()
 
-    # on new attempt
-    if taskSendForm.validate_on_submit():
-        folderpath = path_generate(task.lab_num, task.num, g.user.variant, g.user.cart)
-        files = request.files.getlist("task")
-        dirs_create(folderpath)
-        clear_dir(folderpath)
-        for file in files:
-            save(file, folderpath)
-        attempt = Attempt(
-            timestamp = datetime.utcnow(), 
-            author = g.user, 
-            status = check_task(folderpath),
-            task = task)
-        db.session.add(attempt)
-        db.session.commit()
+	# on new attempt
+	if taskSendForm.validate_on_submit():
+		folderpath = path_generate(task.lab_num, task.num, g.user.variant, g.user.cart)
+		files = request.files.getlist("task")
+		dirs_create(folderpath)
+		clear_dir(folderpath)
+		for file in files:
+			save(file, folderpath)
+		attempt = Attempt(
+			timestamp = datetime.utcnow(), 
+			author = g.user, 
+			status = check_task(folderpath),
+			task = task)
+		db.session.add(attempt)
+		db.session.commit()
 
-    # on task add
-    if conditionForm.validate_on_submit():
-        # save condition to file
-        task_text = task_generate(
-            name = conditionForm.name.data,
-            text = conditionForm.condition.data)
-        with open(full_filepath, 'w') as f:
-            f.write(task_text)
-    
-    return render_template('task.html',
-        user = g.user,
-        attempts = Attempt.query.filter_by(user_id = g.user.id, task = task).all()[::-1],
-        task_id = task_id,
-        get_status = get_status,
-        condition = filepath,
-        task_exist = isfile(full_filepath),
-        taskSendForm = taskSendForm,
-        conditionForm = conditionForm)
+	# on task add
+	if conditionForm.validate_on_submit():
+		# save condition to file
+		task_text = task_generate(
+			name = conditionForm.name.data,
+			text = conditionForm.condition.data)
+		with open(full_filepath, 'w') as f:
+			f.write(task_text)
+	
+	return render_template('task.html',
+		user = g.user,
+		attempts = Attempt.query.filter_by(user_id = g.user.id, task = task).all()[::-1],
+		task_id = task_id,
+		get_status = get_status,
+		condition = filepath,
+		task_exist = isfile(full_filepath),
+		taskSendForm = taskSendForm,
+		conditionForm = conditionForm)
 
 @app.route('/add_user', methods=('GET', 'POST'))
 @login_required
 def add_user():
-    userForm = UserUpdateForm()
+	userForm = UserUpdateForm()
 
-    cart = request.args.get('cart')
-    user = User.query.filter_by(cart = cart).first()
-    if not user:
-        user = User()
-        user.cart = cart
-    
-    if  userForm.validate_on_submit():
-            user.fname = userForm.fname.data
-            user.lname = userForm.lname.data
-            user.pname = userForm.pname.data
-            user.course = userForm.course.data
-            user.group = userForm.group.data
-            user.variant = userForm.variant.data
+	cart = request.args.get('cart')
+	user = User.query.filter_by(cart = cart).first()
+	if not user:
+		user = User()
+		user.cart = cart
+	
+	if  userForm.validate_on_submit():
+			user.fname = userForm.fname.data
+			user.lname = userForm.lname.data
+			user.pname = userForm.pname.data			
+			
+			if userForm.group.data and userForm.course.data:
+				user.set_group(userForm.course.data, userForm.group.data)
+			else:
+				user.update_groups_list(userForm.group.data)
 
-            db.session.add(user)
-            db.session.commit()
-            
-            return redirect(url_for("user", cart=cart))
+			user.variant = userForm.variant.data
 
-    return render_template('edit.html', 
-        userForm = userForm)
+			db.session.add(user)
+			db.session.commit()
+			
+			return redirect(url_for("user", cart=cart))
+
+	return render_template('edit.html', 
+		userForm = userForm)
+
 
 @app.route('/edit', methods=('GET', 'POST'))
 @login_required
 def edit():
-    userForm = UserUpdateForm()
+	userForm = UserUpdateForm()
 
-    id = request.args.get('id')
-    user = User.query.filter_by(cart = id).first()
-    
-    if user:
-        if  userForm.validate_on_submit():
-                user.fname = userForm.fname.data
-                user.lname = userForm.lname.data
-                user.pname = userForm.pname.data
-                user.course = userForm.course.data
-                user.group = userForm.group.data
-                user.variant = userForm.variant.data
-                db.session.add(user)
-                db.session.commit()
-                return redirect(url_for("user", cart=id))
+	id = request.args.get('id')
+	user = User.query.filter_by(cart = id).first()
 
-        return render_template('edit.html', 
-            userForm = userForm,
-            fname = user.fname,
-            lname = user.lname,
-            pname = user.pname,
-            course = user.course,
-            group = user.group,
-            variant = user.variant)
-    else:
-        return redirect(url_for("index"))
+	groups = user.groups_to_string()
+	
+	course = None
+	group = None
+	
+	if not user.is_prepod():
+		print "Groups of the user: ", user.groups
+		users_group = {}
+		if len(user.groups):
+			users_group = user.groups[0]
+			course = users_group.course
+			group = users_group.group_num
+	else:
+		group = groups
+	
+	if user:
+		if  userForm.validate_on_submit():
+			
+			user.fname = userForm.fname.data
+			user.lname = userForm.lname.data
+			user.pname = userForm.pname.data
+			
+			if userForm.group.data and userForm.course.data:
+				user.set_group(userForm.course.data, userForm.group.data)
+			else:
+				user.update_groups_list(userForm.group.data)
+			
+			user.variant = userForm.variant.data
+
+			db.session.add(user)
+			db.session.commit()
+
+			return redirect(url_for("user", cart=id))
+
+		print "course, group: ", course, group,
+		return render_template('edit.html', 
+			userForm = userForm,
+			is_prepod = user.is_prepod(),
+			fname = user.fname,
+			lname = user.lname,
+			pname = user.pname,
+			course = course,
+			group = group,
+			variant = user.variant)
+	else:
+		return redirect(url_for("index"))
 
 @app.route('/add_task', methods=('GET', 'POST'))
 @login_required
 def add_task():
-    if not g.user.is_prepod():
-        flash('User can\'t add or edit tasks.')
-        return redirect(url_for('index'))
-    
-    conditionForm = ConditionForm()
-    task = 0
-    id = request.args.get('id')
-    lab_num = request.args.get('lab')
-    if lab_num:
-        try:
-            lab = Lab.query.filter_by(num = lab_num).first().add_task()
-            task = Task.query.all()[::-1][0]
-            id = task.id
-        except Exception as e:
-            print "Exception: ", e
+	if not g.user.is_prepod():
+		flash('User can\'t add or edit tasks.')
+		return redirect(url_for('index'))
+	
+	conditionForm = ConditionForm()
+	task = 0
+	id = request.args.get('id')
+	lab_num = request.args.get('lab')
+	if lab_num:
+		try:
+			lab = Lab.query.filter_by(num = lab_num).first().add_task()
+			task = Task.query.all()[::-1][0]
+			id = task.id
+		except Exception as e:
+			print "Exception: ", e
 
-    is_exist = bool(id)
+	is_exist = bool(id)
 
-    task = task or Task.query.filter_by(id = id).first() or Task()
-    
-    filepath = "tasks/" + str(id) + ".html"
-    full_filepath = "app/templates/" + filepath
+	task = task or Task.query.filter_by(id = id).first() or Task()
+	
+	filepath = "tasks/" + str(id) + ".html"
+	full_filepath = "app/templates/" + filepath
 
-    if conditionForm.validate_on_submit():
-        # save condition to file
-        task.lab_num = conditionForm.lab.data
-        task.num = conditionForm.task.data
-        db.session.add(task)
-        db.session.commit()
+	if conditionForm.validate_on_submit():
+		# save condition to file
+		task.lab_num = conditionForm.lab.data
+		task.num = conditionForm.task.data
+		db.session.add(task)
+		db.session.commit()
 
-        task_condition = task_generate(
-            name = conditionForm.name.data,
-            text = request.form['condition'])
+		task_condition = task_generate(
+			name = conditionForm.name.data,
+			text = request.form['condition'])
 
-        with open(full_filepath, 'w') as f:
-            f.write(task_condition)
+		with open(full_filepath, 'w') as f:
+			f.write(task_condition)
 
-        return redirect(url_for('task', task_id = task.id))
+		return redirect(url_for('task', task_id = task.id))
 
-    if is_exist:
-        text = ""
-        name = ""
-        try:
-            with open(full_filepath, 'r') as f:
-                text = f.read().decode('utf-8')
-            name, text = task_regenerate(text)
-        except Exception as e:
-            print "Reading file exception:", e
+	if is_exist:
+		text = ""
+		name = ""
+		try:
+			with open(full_filepath, 'r') as f:
+				text = f.read().decode('utf-8')
+			name, text = task_regenerate(text)
+		except Exception as e:
+			print "Reading file exception:", e
 
-        return render_template('add_task.html', 
-            conditionForm = conditionForm,
-            lab = task.lab_num,
-            num = task.num,
-            name = name,
-            text = text)
-    else:
-        return render_template('add_task.html', 
-            conditionForm = conditionForm)
+		return render_template('add_task.html', 
+			conditionForm = conditionForm,
+			lab = task.lab_num,
+			num = task.num,
+			name = name,
+			text = text)
+	else:
+		return render_template('add_task.html', 
+			conditionForm = conditionForm)
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
+	db.session.rollback()
+	return render_template('500.html'), 500
 
 # TODO: chechbox pass/text login page
