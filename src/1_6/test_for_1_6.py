9b5dff6a5ae4904a388ee935b2a2a727923d8d08
bagxi <bagelwithraisins@gmail.com>
2017-03-01 07:27:09 +0300

    improve #22 bag in this test

--- src/1_6/test_for_1_6.py
+++ src/1_6/test_for_1_6.py
@@ -9,7 +9,7 @@ from shared_for_tests import lab_verification
 import pytest
 
 
-variants = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+variants = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
 expected_results = [
     ({
